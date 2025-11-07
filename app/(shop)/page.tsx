647d40bc4ef60ce540f7cf1c2e6cb73648be644c
Nayib0 <nayibobeso@gmail.com>
2025-11-06 22:41:24 -0500

    creating the project

---
+++ app/(shop)/page.tsx
@@ -0,0 +1,11 @@
+import { titleFont } from "@/config/font";
+// import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className={titleFont.className}>Hola mundo</h1>
+    </main>
+  );
+}
